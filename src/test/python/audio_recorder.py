9c5f93f41d634017086adaddfaefcfb56bf0452e
MattiaRiola <s280169@studenti.polito.it>
2023-06-21 16:12:52 +0200

    ✨ - #1 recorder in action

--- src/test/python/audio_recorder.py
+++ src/test/python/audio_recorder.py
@@ -15,13 +15,13 @@ if input_device_index is None:
 
 #################################
 
-NUM_SAMPLES_TO_RECORD = 2
+NUM_SAMPLES_TO_RECORD = 10
 
 chunk = 1024  # Record in chunks of 1024 samples
 sample_format = pyaudio.paInt16  # 16 bits per sample
 channels = 2
 fs = 44100  # Record at 44100 samples per second
-seconds = 5
+seconds = 4
 
 for i in range(NUM_SAMPLES_TO_RECORD):
     filename = f"./tmp/output_record_{i}.wav"
