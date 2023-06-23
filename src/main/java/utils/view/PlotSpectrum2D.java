f3844823797e8b824b8f8406812b3dcafc833ec2
MattiaRiola <s280169@studenti.polito.it>
2023-06-23 20:28:19 +0200

    🖼️ - change spectrum visualization

--- src/main/java/utils/view/PlotSpectrum2D.java
+++ src/main/java/utils/view/PlotSpectrum2D.java
@@ -44,22 +44,41 @@ public class PlotSpectrum2D extends JPanel{
         frame.setLocation(200, 200);
         frame.setVisible(true);
     }
-    public void paintSpectrum(Complex[][] results, Graphics2D g2d){
+
+    private static double findMaxMagnitude(Complex[][] spectrum) {
+        double max = 0;
+        for (int t = 0; t < spectrum.length; t++) {
+            for (int freq = 0; freq < spectrum[t].length; freq++) {
+                if (spectrum[t][freq].abs() > max) {
+                    max = Math.log(spectrum[t][freq].abs() + 1);
+                }
+            }
+        }
+        return max;
+    }
+
+    public void paintSpectrum(Complex[][] results, Graphics2D g2d) {
+        double magScale = 255 / findMaxMagnitude(results);
         int size = this.getSize().height;
         int blockSizeX = 10;
         int blockSizeY = 10;
-        for(int i = 0; i < results.length; i++) {
+        for (int i = 0; i < results.length; i++) {
             int freq = 1;
-            for(int line = 1; line < size; line++) {
+            for (int line = 1; line < size; line++) {
                 // To get the magnitude of the sound at a given frequency slice
                 // get the abs() from the complex number.
                 // In this case I use Math.log to get a more managable number (used for color)
-                double magnitude = Math.log(results[i][freq].abs()+1);
-
+                double magnitude = Math.log(results[i][freq].abs() + 1);
+                int scaledMagnitude = Math.min((int) (magnitude * magScale), 255);
                 // The more blue in the color the more intensity for a given frequency point:
-                g2d.setColor(new Color(0,(int)magnitude*10,(int)magnitude*20));
+                try {
+                    g2d.setColor(new Color(0, scaledMagnitude, scaledMagnitude));
+                } catch (Exception e) {
+                    System.err.println("magnitude: " + magnitude + "\n scaledMagnitude: " + scaledMagnitude);
+                    throw e;
+                }
                 // Fill:
-                g2d.fillRect(i*blockSizeX, (size-line)*blockSizeY,blockSizeX,blockSizeY);
+                g2d.fillRect(i * blockSizeX, (size - line) * blockSizeY, blockSizeX, blockSizeY);
 
                 // I used a improviced logarithmic scale and normal scale:
                 if (logModeEnabled && (Math.log10(line) * Math.log10(line)) > 1) {
