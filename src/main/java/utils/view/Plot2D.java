f15f65919f32094134f7c5e0a593d0d49ad1a0bb
MattiaRiola <s280169@studenti.polito.it>
2023-06-24 11:56:34 +0200

    ♻️ - plot 2d refactor

--- src/main/java/utils/view/Plot2D.java
+++ src/main/java/utils/view/Plot2D.java
@@ -33,20 +33,12 @@ public class Plot2D extends JPanel{
         //create instance of the Graphics to use its methods
         super.paintComponent(grf);
         Graphics2D graph = (Graphics2D)grf;
-        graph.setColor(Color.BLACK);
-
-        //Sets the value of a single preference for the rendering algorithms.
-        graph.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
         // get width and height
         int width = getWidth();
         int height = getHeight();
         double offset = (double) height /2;
 
-
-        // draw graph
-        graph.draw(new Line2D.Double(marg, marg, marg, height-marg));
-        graph.draw(new Line2D.Double(marg, height-marg-offset, width-marg,height-marg-offset));
+        paintGraph(graph, width, height, offset);
 
         //find value of x and scale to plot points
         double x = (double)(width-2*marg)/(cord.length-1);
@@ -58,9 +50,9 @@ public class Plot2D extends JPanel{
         // set points to the graph
         for(int i=0; i<cord.length; i++){
             double x1 = marg+i*x;
-            double y1 = height-marg-scale*cord[i]-offset;
-            graph.fill(new Ellipse2D.Double(x1-2, y1-2, 4, 4));
-            if(i>0) {
+            double y1 = height - marg - scale * cord[i] - offset;
+            graph.fill(new Ellipse2D.Double(x1 - 2, y1 - 2, 4, 4));
+            if (i > 0) {
                 double prevX = marg + x * (i - 1);
                 double prevY = height - marg - scale * cord[i - 1] - offset;
                 graph.drawLine((int) (prevX), (int) (prevY), (int) (x1), (int) (y1));
@@ -68,11 +60,23 @@ public class Plot2D extends JPanel{
         }
     }
 
+    private void paintGraph(Graphics2D graph, int width, int height, double offset) {
+        graph.setColor(Color.BLACK);
+
+        //Sets the value of a single preference for the rendering algorithms.
+        graph.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+
+        // draw graph
+        graph.draw(new Line2D.Double(marg, marg, marg, height - marg));
+        graph.draw(new Line2D.Double(marg, height - marg - offset, width - marg, height - marg - offset));
+    }
+
     //create getMax() method to find maximum value
-    private int getMax(){
+    private int getMax() {
         int max = -Integer.MAX_VALUE;
-        for(int i=0; i<cord.length; i++){
-            if(Math.abs(cord[i])>max)
+        for (int i = 0; i < cord.length; i++) {
+            if (Math.abs(cord[i]) > max)
                 max = Math.abs(cord[i]);
 
         }
@@ -86,9 +90,9 @@ public class Plot2D extends JPanel{
         JFrame frame = new JFrame();
         // set size, layout and location for frame.
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(new Plot2D(data,color));
-        frame.setSize(600, 600);
+        frame.setSize(900, 600);
         frame.setLocation(200, 200);
+        frame.add(new Plot2D(data, color));
         frame.setVisible(true);
     }
     public static void plot2Array(int[] data1,int[] data2){
