1e85e13190d3669734580261f959bfbacf58915a
MattiaRiola <s280169@studenti.polito.it>
2023-06-26 20:26:18 +0200

    🧪 - improve audio recorder, now it stops when a SIGINT or SIGTERM is sent to the script

--- src/test/python/audio_recorder.py
+++ src/test/python/audio_recorder.py
@@ -1,7 +1,55 @@
 import getopt
 import pyaudio
+import signal
 import sys
 import wave
+
+# Declare global variables
+p = pyaudio.PyAudio()
+stream = None
+frames = []
+fs = 44100  # Record at 44100 samples per second
+channels = 2
+chunk = 44100  # Record in chunks of 1024 samples
+sample_format = pyaudio.paInt16  # 16 bits per sample
+NUM_SAMPLES_TO_RECORD = 10
+filename = f"./src/test/resources/audio/game/records/output_record.wav"
+
+
+#################################
+
+
+def signal_handler(sig, frame):
+    # This function will be called when the script is being terminated
+    global p, stream, frames, channels, sample_format, fs, filename
+    # This function will be called when the script is being terminated
+    # Stop and close the stream
+    if stream is not None:
+        stream.stop_stream()
+        stream.close()
+
+    # Terminate the PortAudio interface
+    if p is not None:
+        p.terminate()
+
+    print('Finished recording')
+
+    # Save the recorded data as a WAV file
+    if frames:
+        wf = wave.open(filename, 'wb')
+        wf.setnchannels(channels)
+        wf.setsampwidth(p.get_sample_size(sample_format))
+        wf.setframerate(fs)
+        wf.writeframes(b''.join(frames))
+        wf.close()
+        print('Saved file ', filename)
+
+    sys.exit(0)  # terminate the process
+
+
+# These lines set up the signal handler
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
 
 
 def argReader(argv):
@@ -26,72 +74,62 @@ def argReader(argv):
     return recordSize, numRecords
 
 
-def record(seconds, numRecords):
-    p = pyaudio.PyAudio()
-    #################################
-
-    NUM_SAMPLES_TO_RECORD = 10
-
-    chunk = 44100  # Record in chunks of 1024 samples
-    sample_format = pyaudio.paInt16  # 16 bits per sample
-    channels = 2
-    fs = 44100  # Record at 44100 samples per second
-
-    argReader(sys.argv)
-
-    input_device_index = None
-    for i in range(p.get_device_count()):
-        dev = p.get_device_info_by_index(i)
-        if dev['name'].lower().startswith("stereo mix") or dev['name'].lower().startswith("what u hear"):
-            input_device_index = i
-            print("input selected: ", dev['name'])
-            break
-    if input_device_index is None:
-        raise ValueError("No 'Stereo Mix' or 'What U Hear' input device found.")
-
-    for i in range(NUM_SAMPLES_TO_RECORD):
-        filename = f"./src/test/resources/audio/game/records/output_record_{i}.wav"
-
-        p = pyaudio.PyAudio()  # Create an interface to PortAudio
-
-        print('Recording ', numRecords, ' long ', seconds, ' seconds')
-
-        stream = p.open(format=sample_format,
-                        channels=channels,
-                        rate=fs,
-                        frames_per_buffer=chunk,
-                        input=True,
-                        input_device_index=input_device_index)
-
-        frames = []  # Initialize array to store frames
-        print(type(fs))  # should print <class 'int'> or <class 'float'>
-        print(type(chunk))  # should print <class 'int'> or <class 'float'>
-        print(type(seconds))  # should print <class 'int'> or <class 'float'>
-        # Store data in chunks for 3 seconds
-        for i in range(0, int(fs / chunk * seconds)):
-            data = stream.read(chunk)
-            frames.append(data)
-
-        # Stop and close the stream
-        stream.stop_stream()
-        stream.close()
-        # Terminate the PortAudio interface
-        p.terminate()
-
-        print('Finished recording')
-
-        # Save the recorded data as a WAV file
-        wf = wave.open(filename, 'wb')
-        wf.setnchannels(channels)
-        wf.setsampwidth(p.get_sample_size(sample_format))
-        wf.setframerate(fs)
-        wf.writeframes(b''.join(frames))
-        wf.close()
-        print('Saved file ', filename)
-
-
 recordSize, numRecords = 15, 1
 if __name__ == "__main__":
     recordSize, numRecords = argReader(sys.argv[1:])
 
-record(recordSize, numRecords)
+argReader(sys.argv)
+
+input_device_index = None
+for i in range(p.get_device_count()):
+    dev = p.get_device_info_by_index(i)
+    if dev['name'].lower().startswith("stereo mix") or dev['name'].lower().startswith("what u hear"):
+        input_device_index = i
+        print("input selected: ", dev['name'])
+        break
+if input_device_index is None:
+    raise ValueError("No 'Stereo Mix' or 'What U Hear' input device found.")
+
+p = pyaudio.PyAudio()  # Create an interface to PortAudio
+
+print('Recording ',
+      #    numRecords,
+      #   ' long ',
+      #   recordSize,
+      #   ' seconds'
+      '...')
+
+stream = p.open(format=sample_format,
+                channels=channels,
+                rate=fs,
+                frames_per_buffer=chunk,
+                input=True,
+                input_device_index=input_device_index)
+
+frames = []  # Initialize array to store frames
+# Store data in chunks for 3 seconds
+t = 0
+for t in range(0, int(fs / chunk * recordSize)):
+    data = stream.read(chunk)
+    frames.append(data)
+
+
+def close_and_save_record():
+    # Stop and close the stream
+    stream.stop_stream()
+    stream.close()
+    # Terminate the PortAudio interface
+    p.terminate()
+    print('Finished recording')
+    print('Saving file ', filename, ' ... ')
+    # Save the recorded data as a WAV file
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+    print('Saved file ', filename)
+
+
+close_and_save_record()
